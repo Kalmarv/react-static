0343a40537d5ffdda19000c46797da43aaccead2
Kalmarv <59708937+Kalmarv@users.noreply.github.com>
2022-05-25 19:47:54 -0600

    Add files

---
+++ src/routes/Home.tsx
@@ -0,0 +1,23 @@
+const Home = () => {
+  return (
+    <div className="hero">
+      <div className="hero-content mt-16 text-center">
+        <div className="max-w-md">
+          <h1 className="text-5xl font-bold">Hey There!</h1>
+          <p className="py-6">
+            I wrote some tools to help you convert sensitivity values between different games, generate random
+            sensitivities and more, check it out.
+          </p>
+          <p>
+            If you're interested in how it was built, or want to give it a ⭐, check it out on{' '}
+            <a href="https://github.com/Kalmarv/SensitivityUtils" className="link">
+              GitHub
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
