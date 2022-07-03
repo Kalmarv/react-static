b8324da2a798cbfcdb2f1c0fd288f9243445211d
Kalmarv <59708937+Kalmarv@users.noreply.github.com>
2022-07-02 21:55:57 -0500

    🔧 Add ESlint

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:import/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'jsx-a11y'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+  },
+}
